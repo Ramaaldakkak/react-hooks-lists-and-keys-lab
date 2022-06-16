9aa66db20f12c2486fc3cb5ec0239a859eb5e454
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-06-16 10:35:35 +0300

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+// In the `ProjectList` component, a prop of `projects` is being passed down from the `App` component with an array of objects. For each object in the array, render one `ProjectItem` component with the correct props. Use the `id` of the project for the `key` prop.
+
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map(project => (
+          <ProjectItem key={project.id} {...project} />
+        ))}
+      </div>
     </div>
   );
 }
